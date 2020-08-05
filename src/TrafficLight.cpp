9a480868a4043554ac197afe526b2d1ac2dc1ed6
sofionic <undogv@gmail.com>
2020-08-05 18:22:12 +0000

    Tasks Completed

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,32 +1,45 @@
 #include <iostream>
 #include <random>
 #include "TrafficLight.h"
+#include <chrono>
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+
 template <typename T>
-T MessageQueue<T>::receive()
+TrafficLightPhase MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
+        std::unique_lock<std::mutex> uLock(_mutex);
+        _condition.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
+
+        // remove last vector element from queue
+        TrafficLightPhase msg = std::move(_queue.back());
+        _queue.pop_back();
+
+        return msg; //
 }
 
 template <typename T>
-void MessageQueue<T>::send(T &&msg)
+void MessageQueue<T>::send(TrafficLightPhase &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> uLock(_mutex);
+    _queue.push_back(std::move(msg));
+    _condition.notify_one();
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+ 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
+    _type = ObjectType::objectTrafficLight;
 }
 
 void TrafficLight::waitForGreen()
@@ -34,6 +47,14 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+      while(true)
+    {
+        TrafficLightPhase phase = _messageQueue.receive();
+        if (phase == TrafficLightPhase::green)
+        {
+           return;
+        }
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -44,6 +65,7 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+      threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -53,6 +75,27 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    auto t_0 = std::chrono::high_resolution_clock::now();
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distr(4000, 6000);
+      
+      std::this_thread::sleep_for(std::chrono::milliseconds(distr(gen)));
+      
+      if (_currentPhase == TrafficLightPhase::red)
+      {
+          _currentPhase == TrafficLightPhase::green;
+      }
+      else 
+      {
+          _currentPhase == TrafficLightPhase::red;
+      }
+      
+      _messageQueue.send(std::move(_currentPhase));
+    } 
 }
 
-*/
--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -15,12 +15,19 @@ class Vehicle;
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
 // Also, there should be an std::condition_variable as well as an std::mutex as private members. 
 
+enum  TrafficLightPhase {red=0, green};
+
 template <class T>
 class MessageQueue
 {
 public:
+   TrafficLightPhase receive();
+   void send(TrafficLightPhase &&msg);
 
 private:
+   std::deque<TrafficLightPhase> _queue;
+   std::condition_variable _condition;
+   std::mutex _mutex;
     
 };
 
@@ -30,24 +37,32 @@ private:
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
 
-class TrafficLight
+class TrafficLight : public TrafficObject
 {
 public:
     // constructor / desctructor
+   TrafficLight();
 
     // getters / setters
+    TrafficLightPhase getCurrentPhase();
 
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
 
 private:
     // typical behaviour methods
+    void cycleThroughPhases();
+    TrafficLightPhase _currentPhase=red;
 
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
 
+    MessageQueue<TrafficLightPhase> _messageQueue ;
     std::condition_variable _condition;
     std::mutex _mutex;
+     
 };
 
 #endif
